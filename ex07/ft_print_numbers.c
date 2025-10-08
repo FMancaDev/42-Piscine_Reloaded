6efa4abb25c90bc7e2a86062e03aa17ed93c6b90
Fode Manca <fomanca@c1r2s5.42porto.com>
2025-10-08 20:52:07 +0100

    Display digits 0 through 9

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fomanca <fomanca@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 19:45:49 by fomanca           #+#    #+#             */
+/*   Updated: 2025/10/06 19:51:58 by fomanca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}

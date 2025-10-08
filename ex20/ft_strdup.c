2e0243f98472f372094695d53e399c462570ac3f
Fode Manca <fomanca@c1r2s5.42porto.com>
2025-10-08 20:58:23 +0100

    Duplicate string with malloc

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fomanca <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 15:33:27 by fomanca           #+#    #+#             */
+/*   Updated: 2025/10/08 15:56:41 by fomanca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	while (src[len])
+	{
+		len++;
+	}
+	dest = (char *) malloc (sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
